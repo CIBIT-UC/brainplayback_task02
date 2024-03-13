63f9bf3680c7fdc900bfc1ac990e38a9b5ce151d
Alexandre Sayal <alexandresayal@gmail.com>
2024-03-13 17:27:04 +0000

    trying to discriminate factors

---
+++ mvpa/mvpa_lsa_factors.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=mvpa-lsa-factors
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --mem=256G
+#SBATCH --partition=hpc
+#SBATCH --output=/users3/uccibit/alexsayal/BIDS-BRAINPLAYBACK-TASK2/code/log/%x_%A_%a.out
+#SBATCH --error=/users3/uccibit/alexsayal/BIDS-BRAINPLAYBACK-TASK2/code/log/%x_%A_%a.err
+
+# Load conda environment
+.  /users3/uccibit/alexsayal/miniconda3/etc/profile.d/conda.sh
+conda activate mvpa
+
+# Do
+python /users3/uccibit/alexsayal/BIDS-BRAINPLAYBACK-TASK2/code/mvpa/mvpa_lsa_factors.py
+
+echo "Finished with job $SLURM_JOBID"
