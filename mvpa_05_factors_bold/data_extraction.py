9477223c55924e429303aadc8cbdac79820e6b25
Alexandre Sayal <alexandresayal@gmail.com>
2024-06-06 19:03:01 +0100

    new scripts for mvpa_05

--- mvpa_05_factors_bold/data_extraction.py
+++ mvpa_05_factors_bold/data_extraction.py
@@ -1,6 +1,6 @@
 #%% Import libraries
 import os
-from data_extraction_functions import get_mask, clean_func_image, extract_samples_with_atlas, convert_samples_to_features
+from data_extraction_functions import get_mask, edit_events, clean_func_image, extract_samples_with_atlas, convert_samples_to_features
 
 #%% Define paths
 data_root = '/Volumes/T7/BIDS-BRAINPLAYBACK-TASK2'
@@ -11,8 +11,10 @@ output_feat_dir = os.path.join(data_root, 'derivatives', 'mvpa_05_factors_bold',
 
 #%% Iterate on the subjects and runs
 img_mask = get_mask(data_root)
+
 for subject in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17']:
     for run in ['1', '2', '3', '4']:
-        img_crop = clean_func_image(fmriprep_dir, output_func_dir, img_mask, subject, run, False)
-        samples = extract_samples_with_atlas(img_crop, output_samples_dir, 'koelsch', subject, run)
-        convert_samples_to_features(samples, data_root, output_feat_dir, subject, run)
+        img_crop = clean_func_image(fmriprep_dir, output_func_dir, img_mask, subject, run, overwrite=False)
+        samples = extract_samples_with_atlas(img_crop, 'koelsch_spheres_per_voxel', subject, run)
+        events_split = edit_events(data_root, subject, run)
+        convert_samples_to_features(samples, data_root, output_feat_dir, events_split, subject, run)
--- mvpa_05_factors_bold/data_extraction_functions.py
+++ mvpa_05_factors_bold/data_extraction_functions.py
@@ -2,12 +2,24 @@ import os
 import numpy as np
 import pandas as pd
 import nibabel as nb
-from scipy.signal import detrend
-from scipy.stats import zscore
+from nibabel import Nifti1Image
 from scipy.ndimage import binary_dilation
-from nilearn.image import math_img, resample_to_img, crop_img
+from nilearn.image import math_img, resample_to_img, clean_img, binarize_img
 from nilearn.input_data import NiftiMasker, NiftiSpheresMasker, NiftiLabelsMasker
 from nilearn import datasets
+
+def create_brain_mask(data_root, img_func):
+    """ Create MNI-152 template brain mask """
+    print('Creating brain mask...')
+    brain = os.path.join(data_root, 'derivatives', 'mni_icbm152_t1_tal_nlin_asym_09c_mask.nii')
+    img_roi = math_img("img1", img1=brain)
+    img_resampled = resample_to_img(img_roi, img_func)
+    data_binary = np.array(img_resampled.get_fdata()>=0.25, dtype=np.int8)
+    data_dilated = binary_dilation(data_binary, iterations=2).astype(np.int8)
+    img_mask = nb.Nifti1Image(data_dilated, img_resampled.affine, img_resampled.header)
+    img_mask.set_data_dtype('i1')
+    img_mask.to_filename(os.path.join(data_root, 'derivatives', 'mni_icbm152_t1_tal_nlin_asym_09c_mask_dilate_resample_crop.nii.gz'))
+    print('Brain mask created.')
 
 def get_mask(data_root):
     """ Load MNI-152 template brain mask """
@@ -17,56 +29,95 @@ def get_mask(data_root):
     print('Brain mask loaded.')
     return img_mask
 
-def clean_func_image(fmriprep_dir, output_func_dir, img_mask, subject, run, overwrite=False):
+def clean_func_image(fmriprep_dir: str, output_func_dir: str, img_mask: str, subject: str, run: str, overwrite: bool = False) -> Nifti1Image:
+    """
+    Receives a single fmriprep preprocessed functional image and cleans it using nilearn.image.clean_img().
+    It detrends, standardizes, high-pass filters, and removes confounds.
 
+    Args:
+        fmriprep_dir (str): Directory where the fmriprep preprocessed data is stored.
+        output_func_dir (str): Directory where the cleaned functional image will be saved.
+        img_mask (str): Path to the mask image to be used in cleaning.
+        subject (str): Subject identifier.
+        run (str): Run identifier.
+        overwrite (bool): If True, overwrite existing cleaned image. Default is False.
+
+    Returns:
+        nibabel.Nifti1Image: The cleaned functional image.
+
+    Raises:
+        FileNotFoundError: If the required input files are not found.
+        ValueError: If confounds DataFrame is empty or columns to be dropped are missing.
+    """
+
+    # Define the output path for the cleaned functional image
     func_clean_path = os.path.join(output_func_dir, 
                     f'sub-{subject}_ses-01_task-02a_run-{run}_cleaned.nii.gz')
     
-    if os.path.exists(func_clean_path) and overwrite is False:
+    # Check if the cleaned image already exists and overwrite is set to False
+    if os.path.exists(func_clean_path) and not overwrite:
         print(f'Functional image already cleaned for subject {subject}, run {run}.')
         return nb.load(func_clean_path)
 
     print(f'Cleaning functional image for subject {subject}, run {run}...')
 
+    # Define the input path for the preprocessed functional image
     in_file = os.path.join(fmriprep_dir, f'sub-{subject}', 'ses-01',
                         'func', f'sub-{subject}_ses-01_task-02a_run-{run}_space-MNI152NLin2009cAsym_res-2_desc-preproc_bold.nii.gz')
 
-    # Load functional image
-    img_func = nb.load(in_file)
+    # Raise an error if the input functional image does not exist
+    if not os.path.exists(in_file):
+        raise FileNotFoundError(f"Functional image file not found: {in_file}")
 
-    # Detrend and zscore data and save it under a new NIfTI file
-    data = img_func.get_fdata()
-    data = detrend(data)
-    data = np.nan_to_num(zscore(data, axis=0))
-    img_standardized = nb.Nifti1Image(data, img_func.affine, img_func.header)
-
-    # Multiply functional image with mask and crop image
-    img_cleaned = math_img('img1 * img2',
-                        img1=img_standardized, img2=img_mask.slicer[..., None])
-    img_crop = crop_img(img_cleaned)
+    # Define the path for the confounds file
+    confounds_file = os.path.join(fmriprep_dir, f'sub-{subject}', 'ses-01',
+                        'func', f'sub-{subject}_ses-01_task-02a_run-{run}_desc-confounds_timeseries.tsv')
     
-    # Save cleaned image
+    # Raise an error if the confounds file does not exist
+    if not os.path.exists(confounds_file):
+        raise FileNotFoundError(f"Confounds file not found: {confounds_file}")
+
+    # Load the confounds file into a DataFrame
+    confounds = pd.read_csv(confounds_file, sep='\t')
+
+    # Raise an error if the confounds DataFrame is empty
+    if confounds.empty:
+        raise ValueError("Confounds DataFrame is empty.")
+
+    # Drop 'csf_wm' column if it exists
+    if 'csf_wm' in confounds.columns:
+        confounds.drop('csf_wm', axis=1, inplace=True)
+    
+    # Filter the confounds to include only 'csf', 'trans', and 'rot' related columns, and copy the DataFrame to avoid SettingWithCopyWarning
+    confounds = confounds.filter(regex='csf|trans|rot').copy()
+    
+    # Fill any NaN values in the confounds DataFrame with 0
+    confounds.fillna(0, inplace=True)
+
+    # Clean the functional image using the provided mask, detrend, standardize, high-pass filter, and confounds
+    img_clean = clean_img(in_file,
+                          detrend=True,
+                          standardize=True,
+                          confounds=confounds,
+                          high_pass=0.007,
+                          t_r=1,
+                          mask_img=img_mask)
+    
+    # Save the cleaned image to the specified output path
     print(f'Saving cleaned image for subject {subject}, run {run}...')
-    img_crop.to_filename(func_clean_path)
+    img_clean.to_filename(func_clean_path)
 
     print(f'Functional image cleaned for subject {subject}, run {run}.')
 
-    return img_crop
+    return img_clean
 
-def extract_samples_with_atlas(img_crop, output_samples_dir, atlas_name, subject, run):
-    """Extract samples from the functional image using a specific atlas."""
-    sample_file_path = os.path.join(output_samples_dir, 
-                                    f'sub-{subject}_ses-01_task-02a_run-{run}_atlas-{atlas_name}_samples.npy')
+def extract_samples_with_atlas(img_crop, atlas_name, subject, run):
 
-    if os.path.exists(sample_file_path):
-        print(f'Samples already extracted for subject {subject}, run {run}, atlas {atlas_name}.')
-        return np.load(sample_file_path)
-    
     print(f'Extracting samples from atlas {atlas_name} for subject {subject}, run {run}...')
 
     # Load atlas
     if atlas_name == 'pauli':
-        atlas = datasets.fetch_atlas_pauli_2017(version='det') #ToDo: compare with version='prob' using NiftiMapsMasker()
+        atlas = datasets.fetch_atlas_pauli_2017(version='det')
         masker = NiftiLabelsMasker(labels_img=atlas.maps, standardize=False, detrend=False)
 
     elif atlas_name == 'power':
@@ -77,11 +128,20 @@ def extract_samples_with_atlas(img_crop, output_samples_dir, atlas_name, subject
     elif atlas_name == 'koelsch':
         atlas_path = os.path.join(os.getcwd(),'data','koelsch','Meta_analysis_C05_1k_clust_MNI.nii.gz')
         masker = NiftiLabelsMasker(labels_img=atlas_path, standardize=False, detrend=False)
-        
-    samples = masker.fit_transform(img_crop)
+    
+    elif atlas_name == 'koelsch_spheres':
+        atlas_path = os.path.join(os.getcwd(),'data','koelsch','spheres','koelsch_spheres_atlas.nii.gz')
+        masker = NiftiLabelsMasker(labels_img=atlas_path, standardize=False, detrend=False)
 
-    # save samples
-    np.save(sample_file_path, samples)
+    elif atlas_name == 'koelsch_spheres_per_voxel':
+        #atlas_path = os.path.join(os.getcwd(),'data','koelsch','spheres','koelsch_spheres_atlas.nii.gz')
+        atlas_path = '/Users/alexandresayal/GitHub/brainplayback_task02/data/koelsch/spheres/koelsch_spheres_atlas.nii.gz'
+        atlas_resample = resample_to_img(atlas_path, img_crop, interpolation='nearest')
+        atlas_resample_bin = binarize_img(atlas_resample)
+
+        masker = NiftiMasker(mask_img=atlas_resample_bin, standardize=False, detrend=False)
+    
+    samples = masker.fit_transform(img_crop)
 
     print(f'Samples extracted for subject {subject}, run {run}.')
     return samples
@@ -99,8 +159,9 @@ def edit_events(root_dir, subject, run):
     events['onset'] = events['onset'].round(0).astype(int)
     events['duration'] = events['duration'].round(0).astype(int)
 
-    # remove all 'Noise' trials
+    # remove all 'Noise*' trials
     events = events[events.trial_type != 'Noise']
+    events = events[events.trial_type != 'Noise_InterSong']
 
     # rename 'Wonder', 'Transcendence', 'Tenderness', 'Nostalgia' and 'Peacefulness' to 'Sublimity'
     events['trial_type'] = np.where(events['trial_type'].isin(['Wonder', 'Transcendence', 'Tenderness', 'Nostalgia', 'Peacefulness']), 'Sublimity', events['trial_type'])
@@ -111,6 +172,9 @@ def edit_events(root_dir, subject, run):
     # rename 'Tension' and 'Sadness' to 'Unease'
     events['trial_type'] = np.where(events['trial_type'].isin(['Tension', 'Sadness']), 'Unease', events['trial_type'])
 
+    # add the hemodynamic delay of 4 volumes to all onsets
+    events.loc[:, 'onset'] = events.loc[:, 'onset'] + 4
+
     # let's split the music trials into 4 segments of 6 seconds each
     # and the noise trials into 3 segments of 6 seconds each
     # we will use the onsets and durations from the events file to do this
@@ -119,22 +183,19 @@ def edit_events(root_dir, subject, run):
     new_events = pd.DataFrame(columns=['onset', 'duration', 'trial_type'])
 
     # loop through the events and split the trials
-    num_segments = 2
+    num_segments = 4
     for _, row in events.iterrows():
-        for i in range(1, num_segments): 
-            new_events = pd.concat([new_events, pd.DataFrame({'onset': row['onset'] + i*12,
-                                                              'duration': 12,
+        for i in range(num_segments):
+            new_events = pd.concat([new_events, pd.DataFrame({'onset': row['onset'] + i*6,
+                                                              'duration': 6,
                                                               'trial_type': row['trial_type']}, index=[0])], ignore_index=True)
 
     print(f'Events edited for subject {subject}, run {run}.')
     return new_events
 
-def convert_samples_to_features(samples, data_root, output_func_dir, subject, run):
+def convert_samples_to_features(samples, data_root, output_func_dir, events_split, subject, run):
 
     print(f'Converting samples to features for subject {subject}, run {run}...')
-
-    # Load events file
-    events_split = edit_events(data_root, subject, run)
 
     # Initialize features numpy array to store the mean of the samples in each segment
     features = np.zeros((len(events_split), samples.shape[1]))
